4184964be207a207c1cc58b5fd9d7a765c4b40c1
britt1771 <77078670+britt1771@users.noreply.github.com>
2021-01-20 18:48:15 -0800

    module 5 in progress

---
+++ Module 5/loopy_loops.py
@@ -0,0 +1,26 @@
+if __name__ == "__main__":
+
+    #tuple pokemon
+    pokemon = ('picachu', 'charmander', 'bulbasaur')
+
+    #index 1 from pokemon
+    print (pokemon[1])
+
+    #starters and pokemon
+    pokemon_starters = (
+    ('picachu', 'starter1'),
+    ('charmander', 'starter2'),
+    ('bulbasaur', 'starter3')
+    )
+    #ignore next line
+    #poke_starters_from_tuple = dict(pokemon_starters)
+    print (pokemon_starters)
+
+    #name_letters tuple, if i is true/false
+    name = ('brittany')
+    name_tuple = tuple(name)
+    print(name_tuple)
+    print('i' in name)
+
+    #for loop, int 2-10 with range function
+    
